4b0c56774c36743ba9f427556382a2d525d53ed5
Eva Raymond <evathecoderr@outlook.com>
2017-07-13 17:31:49 +0100

    added comments to app.js changed the color of the todo app

--- myToDo/script/app.js
+++ myToDo/script/app.js
@@ -1,55 +1,44 @@
 $(document).ready(function () {
-    // Format of a todo object
-    // var todo1 = {
-    //     activity: 'Gimme a task',
-    //     id: '001',
-    //     created: Date(),
-    //     completed: true,
-    // };
 
-    // var todo2 = {
-    //     activity: 'Create UI for App',
-    //     id: '002',
-    //     created: Date(),
-    //     completed: false,
-    // };
     // An array to store all todos
-    // var todos = [todo1, todo2];
     var todos = [];
 
     //htmlstring for an empty todo
-    var $empty = $(`<p>No task yet.</p>
-                    <hr>`)
+    var $empty = $(`<div class="empty"><p>No task yet.</p>
+                    <hr></div>`)
 
     //Display todos only if there is a task to show
     function renderTodo() {
         //Clear all todos before rendering
-        $('.empty').empty();
         $('.items').empty();
 
-        if (!todos.length) {
-            $('.empty').append($empty)
-        } else {
+        if (!todos.length) {//if no todo - display 'no task yet'
+            $('.items').append($empty)
+        } else {//add and display todo
             for (var i = 0; i < todos.length; i++) {
-                var $todoItem = $(`<div id=${todos[i].id} class="todo ${(todos[i].completed)?"completed":""}">
+                var todoItem = $(`<div id=${todos[i].id} class="todo ${(todos[i].completed)?"completed":""}">
                                 <input class="item-box" type="checkbox" ${(todos[i].completed)?"checked":""}><span>${todos[i].activity}</span>
                                 <i class="pull-right close">x</i>
                             </div>`)
-                $('.items').append($todoItem)
+                $('.items').append(todoItem)
             }
         }
+        //display number of todos
         $('#todo-count').html(todos.length + ' Task' + (todos.length === 1 ? '' : 's'));
-        // addHoverToDOM();
+        //On hover, make delete button visible
         $('.todo').hover(makeVisible);
+        //Mark as completed/uncompleted when checkbox is clicked
         $('.item-box').change(checkChange);
+        //Delete todo when delete button is clicked
         $('.close').click(deleteTodo);
 
     }
-
+    //Make the 'x' button visible when a todo is hovered on
     function makeVisible() {
         $(this).find('.close').toggle()
     }
 
+    //Toggle the state of a todo checkbox when clicked
     function checkChange() {
         var $id = $(this).parent().attr('id');
         todos = todos.map(todo => {
@@ -60,17 +49,22 @@ $(document).ready(function () {
         });
         renderTodo();
     }
-
+    //Delete a todo task on clicking on the 'x' button
     function deleteTodo() {
         var $id = $(this).parent().attr('id');
         todos = todos.filter(todo => todo.id !== $id);
         renderTodo();
     }
 
+    //Submit todo input
     $('#todo-submit').click(function () {
         $('#todo-form').submit()
     })
 
+    //Prevent reloading for each submission, for empty input do nothing,
+    //Add each todo with most current at the top,
+    //Reset todo form input after submission
+    //Render to display changes
     $('#todo-form').submit(function (e) {
         e.preventDefault();
         var $todoTask = $('[name=todo-task]').val();
@@ -80,7 +74,7 @@ $(document).ready(function () {
 
         renderTodo();
     })
-
+    //Create a todo object using input from form
     function createTodo(task) {
         return {
             activity: task,
@@ -90,7 +84,7 @@ $(document).ready(function () {
         }
 
     }
-
+    //render todo when DOM is ready
     renderTodo();
 
 })
