87e66531ad6be08426efc6c87b4ef725b7ee118a
Eva Raymond <evathecoderr@outlook.com>
2017-07-14 13:21:33 +0100

    Can filter by all completed active states Can edit/change todo but 2 issues jquery focusout event doens't work and doubleclick on a currently editing task triggers another input textbox

--- myToDo/script/app.js
+++ myToDo/script/app.js
@@ -2,22 +2,36 @@ $(document).ready(function () {
 
     // An array to store all todos
     var todos = [];
+    var state = '';
 
     //htmlstring for an empty todo
     var $empty = $(`<div class="empty"><p>No task yet.</p>
                     <hr></div>`)
 
+    function filterByState(todo){
+        if(state){//either state is active or completed
+            if(state === 'completed') //completed
+                return todo.completed;
+            else return !todo.completed; //active
+        }else return true; //default : all todos
+    }
     //Display todos only if there is a task to show
-    function renderTodo() {
+    function renderTodo(todos) {
+
+        // var todos = t===undefined?todos:t;
+        // if(t){var todos = t}
+
+        todos = todos.filter(filterByState);
+
         //Clear all todos before rendering
         $('.items').empty();
 
-        if (!todos.length) {//if no todo - display 'no task yet'
+        if (!todos.length) { //if no todo - display 'no task yet'
             $('.items').append($empty)
-        } else {//add and display todo
+        } else { //add and display todo
             for (var i = 0; i < todos.length; i++) {
                 var todoItem = $(`<div id=${todos[i].id} class="todo ${(todos[i].completed)?"completed":""}">
-                                <input class="item-box" type="checkbox" ${(todos[i].completed)?"checked":""}><span>${todos[i].activity}</span>
+                                <input class="item-box" type="checkbox" ${(todos[i].completed)?"checked":""}><span class="activity">${todos[i].activity}</span>
                                 <i class="pull-right close">x</i>
                             </div>`)
                 $('.items').append(todoItem)
@@ -30,7 +44,10 @@ $(document).ready(function () {
         //Mark as completed/uncompleted when checkbox is clicked
         $('.item-box').change(checkChange);
         //Delete todo when delete button is clicked
+        // var $id = $(this).parent().attr('id')
         $('.close').click(deleteTodo);
+        //Edit todo on double-click
+        $('.activity').dblclick(editTodo);
 
     }
     //Make the 'x' button visible when a todo is hovered on
@@ -47,13 +64,55 @@ $(document).ready(function () {
             }
             return todo;
         });
-        renderTodo();
+        renderTodo(todos);
+    }
+
+    //Edit or make changes to a todo
+    function editTodo() {
+        var $id = $(this).parent().attr('id');
+        console.log('id in ediTodo is ' + $id)
+        var task = $(this).html();
+        $(this).html(`<input class="item-box" id="newTask" value=${task} type="text">`);
+        $('#newTask').keypress(e=>{
+            if(e.keyCode === 13){
+                // console.log($('#newTask').val())
+                editOrDel($id,$('#newTask').val())}
+        })
+        $('#newTask').focusout(e=>editOrDel($id,$('#newTask').val()))
+
+        //Edit todo or delete todo if editing to empty
+        function editOrDel(id,value){
+            console.log('value is ' + value)
+            console.log('id in editordel is ' + id)
+            return value?editor(id,value):del(id)
+        }
+
+        //Helper edit function to change only the todo and date created
+        // Id and checked state remains unchanged
+        function editor($id, $value) {
+            console.log('running editor with value of: ' + $value)
+            todos = todos.map(todo => {
+                if (todo.id === $id) {
+                    todo.activity = $value;
+                    todo.created = Date();
+                }
+                return todo;
+            })
+            renderTodo(todos);
+        }
+        
     }
     //Delete a todo task on clicking on the 'x' button
-    function deleteTodo() {
+    function deleteTodo() {        
         var $id = $(this).parent().attr('id');
+        console.log('id in deleteTodo is ' + $id )//+ ' parent is ' + $(this).parent());
+        del($id);
+        
+    }
+    //Helper delete function to render without deleted item
+    function del($id){
         todos = todos.filter(todo => todo.id !== $id);
-        renderTodo();
+        renderTodo(todos);
     }
 
     //Submit todo input
@@ -72,7 +131,7 @@ $(document).ready(function () {
         todos.unshift(createTodo($todoTask));
         $('[name=todo-task]').val('');
 
-        renderTodo();
+        renderTodo(todos);
     })
     //Create a todo object using input from form
     function createTodo(task) {
@@ -85,6 +144,23 @@ $(document).ready(function () {
 
     }
     //render todo when DOM is ready
-    renderTodo();
+    renderTodo(todos);
+
+    $('.filter .all').click(showAll);
+    $('.filter .active').click(showActive);
+    $('.filter .completed').click(showCompleted);
+
+    function showAll(){
+        state = ''
+        renderTodo(todos);
+    }
+    function showCompleted(){
+        state = 'completed'
+        renderTodo(todos);
+    }
+    function showActive(){
+        state = 'active'
+        renderTodo(todos);
+    }
 
 })
